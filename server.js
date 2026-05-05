4913c2729b6dce346499ab6f6f223641b558b08a
Your Name <your.onomee945@.com>
2026-05-05 22:36:31 +0100

    fix: properly start server in root server.js for Render

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-require('./Server/src/server.js');
+const app = require('./Server/src/server.js');
+const connectDB = require('./Server/src/config/db');
+const PORT = process.env.PORT || 5000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Failed to start server:', err);
+    process.exit(1);
+  });
